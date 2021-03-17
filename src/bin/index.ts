8a26e875e46e68169acb85086a58c81f5519045c
hubiqing <hubiqing@jd.com>
2021-03-17 15:04:39 +0800

    log

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -7,24 +7,33 @@ function start() {
     yargs.command('start <zipFile> [patterns..]',
         'compress files',
         {
+            log: {
+                alias: 'l',
+                boolean: true,
+                default: false
+            }
         },
         async (argv) => {
+            const consoleLog = (...args) => {
+                argv.log && console.log(...args)
+            }
             if (((argv.patterns as any) || []).length === 0) {
                 console.log('提示：请填写第二个pattern参数如： **/*.js , 支持多个: *.js *.cs *.html')
                 return
             }
-            const globbyCompress = new GlobbyCompress(argv.zipFile, argv.patterns, { cwd: process.cwd() })
+            argv.zipFile = (argv.zipFile as String).replace(/\.zip$/, '')
+            const globbyCompress = new GlobbyCompress(`${argv.zipFile}.zip`, argv.patterns, { cwd: process.cwd() })
             try {
-                console.log('开始生成压缩包...')
-                console.time('生成压缩包完成，耗时')
+                consoleLog('开始生成压缩包...')
+                argv.log && console.time('生成压缩包完成，耗时')
                 const size: any = await globbyCompress.compress()
-                console.log('压缩包文件地址:' + globbyCompress.zipFilePath)
-                console.log(`文件体积：${Math.ceil(size / 1000)}KB`)
-                console.timeEnd('生成压缩包完成，耗时')
+                consoleLog('压缩包文件地址:' + globbyCompress.zipFilePath)
+                consoleLog(`文件体积：${Math.ceil(size / 1000)}KB`)
+                argv.log && console.timeEnd('生成压缩包完成，耗时')
 
             } catch (e) {
-                console.error(`发生错误${e.message}`)
-                console.error(e.stack)
+                argv.log && console.error(`发生错误${e.message}`)
+                argv.log && console.error(e.stack)
             }
         }).help()
     let argv = yargs.version().argv
